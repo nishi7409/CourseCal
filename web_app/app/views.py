2c3668326d03671b8f0a243b3d1b02978b6e487e
Nishant Srivastava <20250366+nishi7409@users.noreply.github.com>
2020-12-27 21:44:11 +0530

    Update
    - Files created (scripts/template files/etc)
    - Change in database (for now, we'll change it later)

--- web_app/app/views.py
+++ web_app/app/views.py
@@ -1,3 +1,20 @@
+from django.http import HttpResponse, HttpRequest, HttpResponseServerError, HttpResponseRedirect
+from django.shortcuts import render, redirect
 from django.shortcuts import render
+from django.conf import settings
+from .models import *
+from django import views
+from django.core import mail
+from django.utils import timezone
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import redirect, get_object_or_404
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django import forms
+import json
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html', {})
